d4664163d33cb41f333ab409afdf5b2c98a88ff5
John Baxter <jbaxter@a-zdarts.com>
2020-12-19 09:44:03 -0800

    Trying to deploy to heroku

--- server.js
+++ server.js
@@ -15,8 +15,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
